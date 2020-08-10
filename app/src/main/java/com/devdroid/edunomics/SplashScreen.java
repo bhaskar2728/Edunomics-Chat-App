9d72ab8e14b940773206c5b3dc7d82127f0456eb
Bhaskar Purohit <bhaskar18bcs@iiitkottayam.ac.in>
2020-08-10 16:27:49 +0530

    fourth commit

--- app/src/main/java/com/devdroid/edunomics/SplashScreen.java
+++ app/src/main/java/com/devdroid/edunomics/SplashScreen.java
@@ -1,10 +1,18 @@
 package com.devdroid.edunomics;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -20,5 +28,24 @@ public class SplashScreen extends AppCompatActivity {
 
             }
         },4000);
+
+
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("TAG", "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+
+                        // Log and toast
+                        Log.d("token:", token);
+                        Toast.makeText(SplashScreen.this, token, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
